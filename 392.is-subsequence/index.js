9ca871bb661140e8cb7b15cbd1a393aa7a79b2de
tiaan-jonker <tiaanj06@gmail.com>
2022-07-11 11:35:20 +1200

    Is substring refactored with comments

--- 392.is-subsequence/index.js
+++ 392.is-subsequence/index.js
@@ -1,23 +1,31 @@
-// Input: str called s, and string called t. 
+// Input: str called s, and string called t.
 // Output: return true if there is a subsequence i.e. ace is a subsequence of abcde
 
+// Runtime: 78ms O(|T|), T is the length of target string
+// Space: 41.5mb O(1)
 
-// Solution: tow pointers
+// Solution: two pointers
 
 function isSubsequence(s, t) {
-  const leftBound = s.length
-  const rightBound = t.length
+  // set the bounds for absolute upper limit
+  let sBound = s.length
+  let tBound = t.length
 
-  let left = 0
-  let right = 0
+  // set two points starting at idx zero for each input string
+  let sIdx = 0
+  let tIdx = 0
 
-  while (left < leftBound && right < rightBound) {
-    if (s.charAt(left) === t.charAt(right)) {
-      left++
+  // check the idx for both strings against their respective bounds
+  while (sIdx < sBound && tIdx < tBound) {
+    // if the curr s is equal to curr t
+    if (s[sIdx] === t[tIdx]) {
+      // increment s idx
+      sIdx++
     }
-
-    right++
+    // if not the same then increment t idx
+    tIdx++
   }
 
-  return left === leftBound
+  // if t contains all of s then s idx should 'hit' its upper bound
+  return sBound === sIdx
 }
